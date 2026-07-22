4c180a31027465e3398ee41d6fbfc865ed3829ab
Raphael Gaschignard <raphael.gaschignard@uptickhq.com>
2026-07-22 12:00:52 +1000

    improve test_1.py

--- test_1.py
+++ test_1.py
@@ -3,11 +3,12 @@ import sys
 
 a = 1
 
-b = 1
+b = 3
 
-if (a + b) > 5:
-    print("Failed!")
-    sys.exit(1)
-else:
-    print("Succeeded!")
-    sys.exit(0)
+if __name__ == "__main__":
+    if (a + b) > 5:
+        print("Failed!")
+        sys.exit(1)
+    else:
+        print("Succeeded!")
+        sys.exit(0)
